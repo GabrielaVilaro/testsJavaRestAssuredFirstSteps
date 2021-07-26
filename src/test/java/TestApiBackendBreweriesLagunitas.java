dfd6026961bf2f495a85d75aa683202686338926
gabriela.vilaro <gabriela.vilaro@ank.app>
2021-07-26 13:49:58 -0300

    add test backend lagunitas

--- src/test/java/TestApiBackendBreweriesLagunitas.java
+++ src/test/java/TestApiBackendBreweriesLagunitas.java
@@ -41,33 +41,15 @@ public class TestApiBackendBreweriesLagunitas {
             }
         }
 
-        LOGGER.info("Se filtra por id, aquellas que contengan de estado California");
-        Response response_state = given()
+        LOGGER.info("Se filtra por id, aquellas que contengan estado California y se validan los datos, id, name, street, phone");
+        given()
                 .get(baseURI + "/12040")
                 .then()
-                .extract()
-                .response();
-
-        JsonPath jsonPath_state = response_state.jsonPath();
-        int count_state = jsonPath.getInt("state.size()");
-        String state = "California";
-
-        for(int i = 0; i < count_state; i++)
-        {
-            String search = jsonPath_state.getString("state["+i+"]");
-            if(search.equalsIgnoreCase(state))
-            {
-                LOGGER.info("Se validan los datos");
-                jsonPath_state.getString("state["+i+"]");
-                response_state
-                        .then()
-                        .body("id", equalTo(12040))
-                        .body("name", equalTo("Lagunitas Brewing Co"))
-                        .body("street", equalTo("1280 N McDowell Blvd"))
-                        .body("phone", equalTo("7077694495"));
+                .body("id", equalTo(12040))
+                .body("name", equalTo("Lagunitas Brewing Co"))
+                .body("street", equalTo("1280 N McDowell Blvd"))
+                .body("phone", equalTo("7077694495")).extract().response();
             }
-        }
-    }
 
     @After
     public void afterClass() {
